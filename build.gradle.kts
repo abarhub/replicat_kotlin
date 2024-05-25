6016bdd982eda9a8a4b6794c9d08fdf7a04e5e93
abarhub <abarhub@gmail.com>
2024-05-25 10:36:21 +0200

    ajout d'un serveur

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,11 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("io.javalin:javalin:6.1.3")
+    implementation("org.slf4j:slf4j-simple:2.0.10")
+    implementation("org.apache.logging.log4j:log4j-api:2.7")
+    implementation("org.apache.logging.log4j:log4j-core:2.7")
+    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.7")
 }
 
 tasks.test {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,13 @@
 package org.testspringboot3
 
+import io.javalin.Javalin
+
 fun main() {
     println("Hello World!")
+    var app = Javalin.create(/*config*/)
+        .get("/") { ctx -> ctx.result("Hello World!") }
+//        .get("/", ctx -> {
+//            ctx.result("Hello World")
+//        })
+    .start(7070);
 }
